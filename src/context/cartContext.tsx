16fe981458b8f8ce06b2205a26899bbde2fe818e
Chris Nanninga <chris.nanninga@bigcommerce.com>
2023-12-20 13:38:13 -0600

    Maintain cart state on cookie

--- src/context/cartContext.tsx
+++ src/context/cartContext.tsx
@@ -1,4 +1,6 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
+import Cookies from "universal-cookie";
+import { bcFetch } from "../utils/bigcommerceGql";
 
 export type Cart = {
     id?: string,
@@ -12,10 +14,50 @@ const emptyCart = {
     totalFormatted: '',
 }
 
+const getCartQuery = `
+query GetCart($entityId: String) {
+    site {
+        cart(entityId: $entityId) {
+            entityId
+            amount {
+                currencyCode
+                value
+            }
+            lineItems {
+                totalQuantity
+            }
+        }
+    }
+}
+`
+
 const CartContext = createContext<{cart: Cart, setCart: (cart: Cart) => void}>({cart: emptyCart, setCart: (cart) => {}});
 
 export const CartProvider = ({children}) => {
-    const [cart, setCart] = useState<Cart>(emptyCart);
+    const [cart, setCartState] = useState<Cart>(emptyCart);
+    const cookies = new Cookies();
+
+    const setCart: (cart: Cart) => void = (cart) => {
+        setCartState(cart);
+        cookies.set('cartId', cart.id, { path: '/' });
+    }
+
+    useEffect(() => {
+        const cartId = cookies.get('cartId');
+        if (cartId) {
+            bcFetch<{entityId: string}>(getCartQuery, {
+                entityId: cartId,
+            }).then(result => {
+                const cart = result.data?.site.cart;
+    
+                setCartState({
+                    id: cart.entityId ?? '',
+                    qty: cart.lineItems?.totalQuantity ?? 0,
+                    totalFormatted: cart.amount?.value ? `${cart.amount.value} ${cart.amount?.currencyCode}` : '',
+                })
+            });
+        }
+    }, []);
 
     return (
         <CartContext.Provider value={{cart, setCart}}>
